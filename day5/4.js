a3526265b97406bd62bd9d0bb9ed8af1d31bccd2
koderoom <koderoom.dev@gmail.com>
2020-08-08 13:31:14 +0530

    5

--- day5/4.js
+++ day5/4.js
@@ -1,14 +1,24 @@
 var counter = 1;
 var likeCounter = 1;
 
+/**
+ * Create a Comment ELment
+ * and
+ * Append to the Comment Box
+ *
+ */
 function commentHere() {
+  // newelement initialization
   let newElement = document.createElement("div");
   newElement.textContent = "comment " + counter;
+
+  // styling
   newElement.style.background = "tomato";
   newElement.style.margin = "4px";
 
-  let commentParent = document.querySelector("#commentBox");
-  commentParent.appendChild(newElement);
+  // appending to the parent box
+  let commentBox = document.querySelector("#commentBox");
+  commentBox.appendChild(newElement);
 
   counter++;
 }
