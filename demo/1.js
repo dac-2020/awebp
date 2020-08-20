9cbedb987e327db14244c9bc4e9ed7c9c8f4a9bb
koderoom <koderoom.dev@gmail.com>
2020-08-20 20:10:51 +0530

    test

---
+++ demo/1.js
@@ -0,0 +1,24 @@
+/**
+ * Package Manager
+ * Hello World
+ * Project strucure.
+ * Import Export Local Module Management
+ * File System Module
+ * Debugging
+ * Working With Databases
+ * Collections
+ * Promise
+ * Async Await
+ * Typescript - Module Managment
+ * Working with Database
+ * Express JS
+ * Express JS with Database
+ */
+
+class Hello {
+  static main() {
+    console.log("Hello World");
+  }
+}
+
+Hello.main();
